b58c8132c425c1f2f8dd648ed939e7a409c2c333
Elaria Basher <elariabasher@gmail.com>
2025-06-25 00:36:04 +0300

    Update login.js.js

--- login.js.js
+++ login.js.js
@@ -1,16 +1,2 @@
-let namee = document.getElementById("namie");
-let g_mail = document.getElementById("mail");
-function show() {
-  if (namee.value.trim() == "") {
-    alert("empty name");
-    return;
-  }
-  if (g_mail.value.trim() == "") {
-    alert("empty mail");
-    return;
-  }
 
-alert("Thank you " + namee.value + ", your email " + g_mail.value + " has been saved!");
-window.open("https://www.google.com","_self")
-}
 
